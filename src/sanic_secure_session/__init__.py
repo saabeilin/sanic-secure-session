fd98fd2e2fff674d5cbecf07899d3dfac0369212
Sergei Beilin <saabeilin@gmail.com>
2017-12-22 19:32:10 +0100

    Sign cookie

--- src/sanic_secure_session/__init__.py
+++ src/sanic_secure_session/__init__.py
@@ -1,3 +1,4 @@
+from itsdangerous import Signer, BadData
 from sanic import Sanic
 
 from sanic_secure_session.backends import FakeStorageBackend
@@ -7,8 +8,9 @@ from sanic_secure_session.session import Session
 class SanicSession:
     cookie_name = 'session'
 
-    def __init__(self, app=None, storage_backend=None):
-        self.storage_backend = None or FakeStorageBackend()
+    def __init__(self, app=None, secret_key=None, storage_backend=None):
+        self.secret_key = secret_key
+        self.storage_backend = storage_backend
         if app is not None:
             self.init_app(app)
 
@@ -21,7 +23,12 @@ class SanicSession:
         # before each request initialize a session
         # using the client's request
         # await session_interface.open(request)
-        sid = request.cookies.get(self.cookie_name)
+        signed_sid = request.cookies.get(self.cookie_name)
+        try:
+            sid = Signer(self.secret_key).unsign(signed_sid).decode('ascii')
+        except BadData:
+            sid = None
+
         request['session'] = await self.storage_backend.load(sid) or Session()
 
     async def save_session(self, request, response):
@@ -29,4 +36,4 @@ class SanicSession:
         # pass the response to set client cookies
         # await session_interface.save(request, response)
         await self.storage_backend.save(request['session'].sid, request['session'])
-        response.cookies[self.cookie_name] = request['session'].sid
+        response.cookies[self.cookie_name] = Signer(self.secret_key).sign(request['session'].sid.encode('ascii')).decode('ascii')
